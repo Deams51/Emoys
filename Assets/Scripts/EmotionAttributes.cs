7bf314b1d15c8d17f844f5ff8ba9f3cba60711d4
Deams51 <mfourgeaud@gmail.com>
2014-10-12 13:14:27 +0200

    Creating prefab wall

---
+++ Assets/Scripts/EmotionAttributes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmotionAttributes : MonoBehaviour {
+
+    public float appeal = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
